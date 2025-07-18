abdd2fa2dff9cd244abc4a832719086dac8f65cb
Mr3rf1 <e811.py@gmail.com>
2025-07-19 01:00:17 +0330

    Update API credentials in reper.py for consistency

--- reper.py
+++ reper.py
@@ -22,8 +22,8 @@ try:
 except: pass
 session_files = listdir('sessions')
 session_files.sort()
-api_id = 2839216
-api_hash = '967fc90f9013e51dd7fe0713c35e28f8'
+api_id = 1234567
+api_hash = '967fc90f90ajfu1dd7fe0724jm5e28f8'
 if command_line_args.help:
     print(f'''Help:
   -an {Fore.LIGHTBLUE_EX}NUMBER{Fore.RESET}, --add-number {Fore.LIGHTBLUE_EX}NUMBER{Fore.RESET} ~> {Fore.YELLOW}add account to script{Fore.RESET}
